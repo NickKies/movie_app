e3261d352a93286e9564d66a849fafd00985289a
YoonSung Kim <opqudk@gmail.com>
2021-06-08 12:27:16 +0900

    useFullscreen

--- src/routes/hooks/UseFullscreen.js
+++ src/routes/hooks/UseFullscreen.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {useRef} from 'react';
+
+const useFullscreen = (callback) => {
+    const element = useRef();
+    const runCb = isFull => {
+        if ( callback && typeof callback === 'function' ) {
+            callback(isFull);
+        }
+    };
+
+    const triggerFull = () => {
+        if ( element.current ) {
+            if ( element.current.requestFullscreen ) {
+                element.current.requestFullscreen();
+            } else if ( element.current.mozRequestFullScreen ) {
+                element.current.mozRequestFullScreen();
+            } else if ( element.current.webkitRequestFullscreen ) {
+                element.current.webkitRequestFullscreen();
+            } else if ( element.current.msRequestFullscreen ) {
+                element.current.msRequestFullscreen();
+            }
+            runCb(true);
+        }
+    };
+    const exitFull = () => {
+        if ( document.exitFullscreen ) {
+            document.exitFullscreen();
+        } else if ( document.mozCancelFullScreen ) {
+            document.mozCancelFullScreen();
+        } else if ( document.webkitExitFullscreen ) {
+            document.webkitExitFullscreen();
+        } else if ( document.msExitFullscreen ) {
+            document.msExitFullscreen();
+        }
+        runCb(false);
+    };
+
+    return { element, triggerFull, exitFull };
+}
 
 const UseFullscreen = () => {
+    const onFullS = isFull => {
+        console.log(isFull? 'full' : 'small');
+    }
+    const {element, triggerFull, exitFull } = useFullscreen(onFullS);
     return (
         <div className="about__container">
-            <h1>Hi</h1>
+            <div ref={element}>
+                <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250"/>
+                <button onClick={exitFull}>Exit fullscreen</button>
+            </div>
+            <button onClick={triggerFull}>Make fullscreen</button>
         </div>
     );
 }
