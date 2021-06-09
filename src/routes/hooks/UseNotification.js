84dc8a92fe33d1054c3d317643342479fd296132
YoonSung Kim <opqudk@gmail.com>
2021-06-09 09:07:04 +0900

    useNotification

--- src/routes/hooks/UseNotification.js
+++ src/routes/hooks/UseNotification.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+const useNotification = (title, options) => {
+    if ( !('Notification' in window) ) {
+        return false;
+    }
+    const fireNotif = () => {
+        if ( Notification.permission !== 'granted' ) {
+            Notification.requestPermission().then(permission => {
+                if ( permission === 'granted' ) {
+                    new Notification(title, options);
+                } else {
+                    return false;
+                }
+            })
+        } else {
+            new Notification(title, options);
+        }
+        return fireNotif;
+    }
+}
+
 const UseNotification = () => {
+    const triggerNotif = useNotification('Can I steal year kimchi?', {body: 'I love kimchi dont u?'});
     return (
         <div className="about__container">
-            <h1>Hi</h1>
+            <button onClick={triggerNotif}>Hi</button>
         </div>
     );
 }
